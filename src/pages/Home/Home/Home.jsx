493571e0bed84b67ff0f1687d21d66f05374f81c
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-05-02 10:59:57 +0600

    banner added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,69 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../../../assets/banner1.jpg'
+import banner2 from '../../../assets/banner2.jpg'
+import banner3 from '../../../assets/banner3.jpg'
 
 const Home = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    }
     return (
         <div>
-            <h2>This is Home</h2>
+            {/* banner */}
+            <div className='container'>
+                <Carousel activeIndex={index} onSelect={handleSelect}>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 "
+                            src={banner1}
+                            alt="First slide"
+                            style={{height: '500px'}}
+                        />
+                        <Carousel.Caption>
+                            <h3>Barbique Lover</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <button className='btn btn-primary'>Place your order</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 "
+                            src=
+                            {banner2}
+                            alt="Second slide"
+                            style={{height: '500px'}}
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Kabbab Heaven</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <button className='btn btn-primary'>Place your order</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src=
+                            {banner3}
+                            alt="Third slide"
+                            style={{height: '500px'}}
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Spice Lover</h3>
+                            <p>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            </p>
+                            <button className='btn btn-primary'>Place your order</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,33 +1,41 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import Marquee from 'react-fast-marquee';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">Recipe Heaven</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <div className='container'>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">Recipe Heaven</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">Blog</Nav.Link>
+                        
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                               
+                            <Button variant="secondary">Login</Button>{' '}
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <div className='d-flex mt-2 mb-2'>
+                <Button variant="danger">Open</Button>
+                <Marquee className='text-danger' speed={100}>
+                    24 Hours Open!!!! place your order now..... 24 Hours Open!!!! place your order now
+                </Marquee>
+            </div>
+        </div>
+
     );
 };
 
