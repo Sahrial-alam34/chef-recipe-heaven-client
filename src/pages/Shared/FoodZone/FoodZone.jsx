dc64b46460b76dbeace245cd003a7fe19f2df2a9
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-05-02 11:19:43 +0600

    left nav bar added

---
+++ src/pages/Shared/FoodZone/FoodZone.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import foodzone1 from '../../../assets/foodzone1.jpg'
+import foodzone2 from '../../../assets/foodzone2.jpg'
+import foodzone3 from '../../../assets/foodzone3.jpg'
+
+const FoodZone = () => {
+    return (
+        <div className='bg-light text-center my-4 py-4 '>
+            <h4>Food Zone</h4>
+            <div >
+                <img className='mb-2' src={foodzone1} style={{width:'275px',height:'180px'}} alt="" />
+                <img className='mb-2' src={foodzone2} alt="" />
+                <img className='mb-2' src={foodzone2} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default FoodZone;
