bfbbd1ddfe256da2776a3fc04e6d0ad82fad3ca3
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-05-04 12:45:34 +0600

    loading spinner added

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -13,22 +13,27 @@ const AuthProviders = ({children}) => {
     const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) =>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email,password)
     }
 
     const signIn = (email, password) =>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const signInWithGoogle =  () =>{
+        setLoading(true);
         return signInWithPopup(auth, googleAuthProvider);
     }
 
     const signInWithGithub = () =>{
+        setLoading(true);
         signInWithPopup(auth, githubAuthProvider)
     }
 
     const logOut = () =>{
+        setLoading(true);
         signOut(auth);
     }
 
@@ -48,12 +53,13 @@ const AuthProviders = ({children}) => {
     },[])
     const authInfo= {
         user,
+        loading,
         createUser,
         signIn,
         signInWithGoogle,
         signInWithGithub,
         logOut,
-        loading
+        
       
     }
     return (
